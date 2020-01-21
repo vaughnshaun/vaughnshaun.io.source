9650881ce26962ceb0d36550ed06728a6fbf8b8d
svaughn <shaun.vaughn@rockdovesolutions.com>
2020-01-20 19:34:12 -0500

    Updates

--- src/components/CategoryList.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const CategoryList = ({children}) => <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>{children}</div>;
---
+++ styles-2995dfc2d3ec713d1cab.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"7iQ6":function(n,o,i){},"8ypT":function(n,o,i){},q4sD:function(n,o,i){}}]);
+//# sourceMappingURL=styles-2995dfc2d3ec713d1cab.js.map
