8e0ff2ce27f530853362362cd47e60d68c7b9f84
svaughn <shaun.vaughn@rockdovesolutions.com>
2020-01-22 08:35:27 -0500

    Added font tweaks and removed header because I don't have a need for navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         start_url: `/`,
         //background_color: `#663399`,
         //theme_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#0f7bc1`,
         background_color: `#0f7bc1`,
         display: `minimal-ui`/*,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.*/
